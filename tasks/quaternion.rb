592bef91d69fb98844fd47b042fb1897f17bd070
agalloch <the.dreadful.hours@gmail.com>
2014-10-01 17:13:22 +0300

    solution of task 2: the bad

--- tasks/quaternion.rb
+++ tasks/quaternion.rb
@@ -11,15 +11,16 @@ class Quaternion
     @q[key] = value
   end
 
+  def coordinates
+    @q.values
+  end
+
   def -@
-    Quaternion.new(-self[:e], -self[:i], -self[:j], -self[:k])
+    Quaternion.new(*coordinates.map { |coord| coord * (-1) })
   end
 
   def +(other)
-    Quaternion.new(self[:e] + other[:e],
-      self[:i] + other[:i],
-      self[:j] + other[:j],
-      self[:k] + other[:k])
+    Quaternion.new(*coordinates.zip(other.coordinates).map { |item| item.first + item.last})
   end
 
   def -(other)
@@ -34,15 +35,17 @@ class Quaternion
   end
 
   def conjugate
-    Quaternion.new(self[:e], -self[:i], -self[:j], -self[:k])
+    Quaternion.new(*@q.map { |k, v| (k == :e) ? v : v * (-1) })
   end
 
   def norm
-    Math.sqrt(self[:e]**2 + self[:i]**2 + self[:j]**2 + self[:k]**2)
+    Math.sqrt(coordinates.map { |coord| coord**2 }.reduce(0, :+))
   end
 
   def to_s
-    "#{self.class}: (#{self[:e]}, #{self[:i]}*i, #{self[:j]}*j, #{self[:k]}*k)"
+    formatted_coordinates = @q.map { |k, v| "#{v}#{v.zero? ? '' : k}" }.join(', ')
+
+    "#{self.class}: (#{formatted_coordinates})"
   end
 end
 
@@ -51,12 +54,25 @@ if __FILE__ == $0
   q1 = Quaternion.new(1, 0, 6, 1)
   q2 = Quaternion.new(1, 5, 1, 1)
 
-  p q1 + q2
-  p q1.conjugate
-  q1[:j] = 67
-  puts "#{q1} norm = #{q1.norm}"
+  q_e = Quaternion.new(1, 0, 0, 0)
+  q_i = Quaternion.new(0, 1, 0, 0)
+  q_j = Quaternion.new(0, 0, 1, 0)
+  q_k = Quaternion.new(0, 0, 0, 1)
 
+  puts "ij == #{q_i * q_j} == -#{q_j * q_i}"
+  puts "jk == #{q_j * q_k} == -#{q_k * q_j}"
+  puts "ki == #{q_k * q_i} == -#{q_i * q_k}"
+
+  puts "identity for i #{q_i * q_e} is the same as #{q_e * q_i}"
+  puts "identity for j #{q_j * q_e} is the same as #{q_e * q_j}"
+  puts "identity for k #{q_k * q_e} is the same as #{q_e * q_k}"
+
+  p q1 + q2
   p q1 - q2
+  p (q1 - q2).norm
+
+  p q1.conjugate
+
   p q1 * q2
   p q2 * q1
 end
