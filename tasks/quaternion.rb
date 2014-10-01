6bad66c8d51e1a0be1fb77f7915a67504f4f5f9c
agalloch <the.dreadful.hours@gmail.com>
2014-10-01 11:10:43 +0300

    solution of task 2: the ugly

--- tasks/quaternion.rb
+++ tasks/quaternion.rb
@@ -1,18 +1,44 @@
 class Quaternion
-  def initialize(default)
-    @q = default
+  def initialize(e, i, j, k)
+    @q = {e: e, i: i, j: j, k: k}
   end
 
   def [](component)
     @q[component] ? @q[component] : 0
   end
 
+  def []=(key, value)
+    @q[key] = value
+  end
+
+  def -@
+    Quaternion.new(-self[:e], -self[:i], -self[:j], -self[:k])
+  end
+
   def +(other)
-    Quaternion.new(e: self[:e] + other[:e], i: self[:i] + other[:i], j: self[:j] + other[:j], k: self[:k] + other[:k])
+    Quaternion.new(self[:e] + other[:e],
+      self[:i] + other[:i],
+      self[:j] + other[:j],
+      self[:k] + other[:k])
+  end
+
+  def -(other)
+    self + (-other)
+  end
+
+  def *(other)
+    Quaternion.new(self[:e] * other[:e] - self[:i] * other[:i] - self[:j] * other[:j] - self[:k] * other[:k],
+      self[:e] * other[:i] + self[:i] * other[:e] + self[:j] * other[:k] - self[:k] * other[:j],
+      self[:e] * other[:j] - self[:i] * other[:k] + self[:j] * other[:e] + self[:k] * other[:i],
+      self[:e] * other[:k] + self[:i] * other[:j] - self[:j] * other[:i] + self[:k] * other[:e])
   end
 
   def conjugate
-    Quaternion.new(e: self[:e], i: -self[:i], j: -self[:j], k: -self[:k])
+    Quaternion.new(self[:e], -self[:i], -self[:j], -self[:k])
+  end
+
+  def norm
+    Math.sqrt(self[:e]**2 + self[:i]**2 + self[:j]**2 + self[:k]**2)
   end
 
   def to_s
@@ -20,10 +46,17 @@ class Quaternion
   end
 end
 
-if __FILE__ == $0
-  q1 = Quaternion.new(e: 1, i: 6, k: 1)
-  q2 = Quaternion.new(e: 1, i: 5, j: 1, k: 1)
 
-  p (q1 + q2)
+if __FILE__ == $0
+  q1 = Quaternion.new(1, 0, 6, 1)
+  q2 = Quaternion.new(1, 5, 1, 1)
+
+  p q1 + q2
   p q1.conjugate
+  q1[:j] = 67
+  puts "#{q1} norm = #{q1.norm}"
+
+  p q1 - q2
+  p q1 * q2
+  p q2 * q1
 end
